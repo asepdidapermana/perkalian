9e2b43224a44acb87842ad2738a4c9189a0be214
unknown <asepdidapermana>
2023-05-08 11:33:08 +0700

    kalkulator

---
+++ kalkulator1.js
@@ -0,0 +1,9 @@
+const angka1 = document.getElementById('angka1')
+const angka2 = document.getElementById('angka2')
+const operator = document.getElementById('operator')
+const hasil = document.getElementById('hasil')
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', function(){
+console.log("helo")
+})
